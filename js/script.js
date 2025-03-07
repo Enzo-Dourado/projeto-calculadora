f4c01f56397d14d3cbe762d79af8bc4902248e54
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-03-07 17:21:25 -0300

    Testes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function num1() {
+    let numero1 = document.getElementsByClassName('num')
+    let tela = document.getElementById('idTela')
+    let n1 = document.createElement('text')
+    n1.text = `numero1`
+    tela.appendChild(numero1.innerHTML = '1')
+}
